651796424dda40a5dc01fceaaf5ef8fa0d4fd348
Patrick Fulton <pfulton@adobe.com>
2022-10-06 16:12:41 -0400

    feat: add withRTL decorator to Storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,5 @@
+import { useEffect } from '@storybook/client-api'
+
 import '../components/vars/dist/spectrum-global.css';
 import '../components/vars/dist/spectrum-darkest.css';
 import '../components/vars/dist/spectrum-dark.css';
@@ -6,6 +8,20 @@ import '../components/vars/dist/spectrum-medium.css';
 import '../components/vars/dist/spectrum-large.css';
 import '../components/tokens/dist/index.css';
 
+export const globalTypes = {
+  textDirection: {
+    name: 'Text direction',
+    description: 'Direction of the text',
+    defaultValue: 'ltr',
+    toolbar: {
+      icon: 'arrowrightalt',
+      items: [
+        { value: 'ltr', icon: 'arrowrightalt', title: 'left to right' },
+        { value: 'rtl', icon: 'arrowleftalt', title: 'right to left' },
+      ]
+    }
+  }
+}
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
@@ -19,3 +35,19 @@ export const parameters = {
     removeEmptyComments: true,
   }
 }
+
+const withRTL = (StoryFn, context) => {
+  const { parameters, globals } = context;
+
+  const defaultDirection = 'rtl';
+  const textDirection = parameters.textDirection || globals.textDirection || defaultDirection;
+
+  useEffect(() => {
+    // Set RTL only if passed as a parameter or toggled via toolbar
+    document.body.dir = textDirection
+  }, [textDirection])
+
+  return StoryFn();
+}
+
+export const decorators = [withRTL]
